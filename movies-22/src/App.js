b9f68aaa11edfba110d515b7e5cdb2f91abda134
Lewis <lewisnpakoti@gmail.com>
2022-11-17 22:43:43 +1300

    added movie listing to App component. Still need to fix display of movies.

--- movies-22/src/App.js
+++ movies-22/src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Movie from './components/Movie';
 
+// Shouldn't have API key in API. This is for test puposes only.
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMAGE_API = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movies?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
 function App() {
-  const movies = ['1', '2', '3']
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      })
+  }, [])
+
   return <div>
-    {movies.map(movie => (
-      <h1>{movie}</h1>
-    ))}
+    {movies.length > 0 && movies.map((movie) => <Movie />)}
   </div>
 }
 
